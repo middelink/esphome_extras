39575b2a592ad4023e79f1104038d422d417a1ff
Pauline Middelink <middelink@polyware.nl>
2023-11-30 14:16:21 +0100

    Fixup apds9301 schema for newer esphome's.

--- components/apds9301/sensor.py
+++ components/apds9301/sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
-from esphome.const import CONF_ID, CONF_RESOLUTION, UNIT_LUX, ICON_BRIGHTNESS_5
+from esphome.const import CONF_ID, CONF_RESOLUTION, UNIT_LUX, ICON_BRIGHTNESS_5, DEVICE_CLASS_ILLUMINANCE, STATE_CLASS_MEASUREMENT
 
 DEPENDENCIES = ['i2c']
 
@@ -18,10 +18,21 @@ APDS9301_RESOLUTIONS = {
 
 APDS9301Sensor = apds9301_ns.class_('APDS9301Sensor', sensor.Sensor, cg.PollingComponent, i2c.I2CDevice)
 
-CONFIG_SCHEMA = sensor.sensor_schema(UNIT_LUX, ICON_BRIGHTNESS_5, 1).extend({
-    cv.GenerateID(): cv.declare_id(APDS9301Sensor),
-    cv.Optional(CONF_RESOLUTION, default=1.0): cv.enum(APDS9301_RESOLUTIONS, float=True),
-}).extend(cv.polling_component_schema('60s')).extend(i2c.i2c_device_schema(0x39))
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        APDS9301Sensor,
+        unit_of_measurement=UNIT_LUX,
+        icon=ICON_BRIGHTNESS_5,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_ILLUMINANCE,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ).extend({
+        #cv.GenerateID(): cv.declare_id(APDS9301Sensor),
+        cv.Optional(CONF_RESOLUTION, default=1.0): cv.enum(APDS9301_RESOLUTIONS, float=True),
+    })
+    .extend(cv.polling_component_schema('60s'))
+    .extend(i2c.i2c_device_schema(0x39))
+)
 
 
 def to_code(config):
